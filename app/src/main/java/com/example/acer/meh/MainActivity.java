382f57108ad5fb8b46ea388ac5539c2e5d5269f8
MiraMeh <amiriaaa2013@gmail.com>
2018-09-06 17:31:20 +0100

    comment I have added

--- app/src/main/java/com/example/acer/meh/MainActivity.java
+++ app/src/main/java/com/example/acer/meh/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //lol i dont know what i am doing but im writing :p
     }
 }
